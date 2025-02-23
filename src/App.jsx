92706aacb31c44f78534515da540b4a8fd2f8a79
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-23 19:20:56 +0100

    basic layout with sections and header for large screen only

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,215 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="header-and-main">
+        <header>
+          <div>
+            <div className="name">Artyom Moskalew</div>
+            <div className="title">Full-stack developer</div>
+            <div className="status">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed,
+              aliquid? Magnam, officiis sunt. Dolore saepe voluptatibus
+              laboriosam, enim fuga, perferendis temporibus nobis soluta minima
+              error odio excepturi ut voluptates sapiente!
+            </div>
+            <nav>
+              <ul>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+              </ul>
+            </nav>
+          </div>
+          <div className="social-media">Social Media</div>
+        </header>
+        <main className="content">
+          <section id="about">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+            molestias illo perspiciatis, nihil expedita id excepturi distinctio,
+            omnis quae ut, ratione enim voluptas dolore sapiente cum nobis
+            aspernatur mollitia hic! Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Reprehenderit molestias vel dignissimos, provident
+            eligendi nihil deserunt, ipsam, earum totam magni reiciendis rem.
+            Maiores obcaecati ab tenetur consectetur, ducimus voluptates
+            necessitatibus. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Voluptatum nostrum aperiam eveniet assumenda praesentium
+            tempore voluptates enim nam quia, impedit sint? Beatae modi vel
+            aperiam voluptatem ex. Pariatur, animi magnam! Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Tenetur consectetur libero
+            earum minima dignissimos eum. Accusantium a mollitia, porro
+            quibusdam minima quod laudantium nemo qui, non, distinctio animi
+            nulla inventore. Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Quae, vero? Minus aliquid molestias autem, consectetur iure
+            laborum fugiat dolorem minima, quas officia cumque, modi tenetur.
+          </section>
+          <section id="experience">
+            Ad molestias numquam ut ea. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolore fugiat consequatur ad quidem sint dolorum
+            deleniti fuga laudantium modi, laboriosam vel optio rem unde animi
+            iusto repellendus repudiandae. Doloremque, optio. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Quod, quaerat! Libero illo
+            delectus, nesciunt voluptatum dolores nobis ab veritatis corporis
+            dolor exercitationem iure eum aliquid consectetur, ad necessitatibus
+            molestiae incidunt! Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Facilis molestias illo perspiciatis, nihil
+            expedita id excepturi distinctio, omnis quae ut, ratione enim
+            voluptas dolore sapiente cum nobis aspernatur mollitia hic! Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+            molestias vel dignissimos, provident eligendi nihil deserunt, ipsam,
+            earum totam magni reiciendis rem. Maiores obcaecati ab tenetur
+            consectetur, ducimus voluptates necessitatibus. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Voluptatum nostrum aperiam
+            eveniet assumenda praesentium tempore voluptates enim nam quia,
+            impedit sint? Beatae modi vel aperiam voluptatem ex. Pariatur, animi
+            magnam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Tenetur consectetur libero earum minima dignissimos eum. Accusantium
+            a mollitia, porro quibusdam minima quod laudantium nemo qui, non,
+            distinctio animi nulla inventore. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quae, vero? Minus aliquid molestias
+            autem, consectetur iure laborum fugiat dolorem minima, quas officia
+            cumque, modi tenetur. Ad molestias numquam ut ea. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Dolore fugiat consequatur ad
+            quidem sint dolorum deleniti fuga laudantium modi, laboriosam vel
+            optio rem unde animi iusto repellendus repudiandae. Doloremque,
+            optio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quod, quaerat! Libero illo delectus, nesciunt voluptatum dolores
+            nobis ab veritatis corporis dolor exercitationem iure eum aliquid
+            consectetur, ad necessitatibus molestiae incidunt!Lorem ipsum dolor
+            sit amet consectetur, adipisicing elit. Facilis molestias illo
+            perspiciatis, nihil expedita id excepturi distinctio, omnis quae ut,
+            ratione enim voluptas dolore sapiente cum nobis aspernatur mollitia
+            hic! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit molestias vel dignissimos, provident eligendi nihil
+            deserunt, ipsam, earum totam magni reiciendis rem. Maiores obcaecati
+            ab tenetur consectetur, ducimus voluptates necessitatibus. Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit.{" "}
+          </section>
+          <section id="projects">
+            Voluptatum nostrum aperiam eveniet assumenda praesentium tempore
+            voluptates enim nam quia, impedit sint? Beatae modi vel aperiam
+            voluptatem ex. Pariatur, animi magnam! Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Tenetur consectetur libero earum
+            minima dignissimos eum. Accusantium a mollitia, porro quibusdam
+            minima quod laudantium nemo qui, non, distinctio animi nulla
+            inventore. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quae, vero? Minus aliquid molestias autem, consectetur iure laborum
+            fugiat dolorem minima, quas officia cumque, modi tenetur. Ad
+            molestias numquam ut ea. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Dolore fugiat consequatur ad quidem sint dolorum
+            deleniti fuga laudantium modi, laboriosam vel optio rem unde animi
+            iusto repellendus repudiandae. Doloremque, optio. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Quod, quaerat! Libero illo
+            delectus, nesciunt voluptatum dolores nobis ab veritatis corporis
+            dolor exercitationem iure eum aliquid consectetur, ad necessitatibus
+            molestiae incidunt!Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Facilis molestias illo perspiciatis, nihil
+            expedita id excepturi distinctio, omnis quae ut, ratione enim
+            voluptas dolore sapiente cum nobis aspernatur mollitia hic! Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+            molestias vel dignissimos, provident eligendi nihil deserunt, ipsam,
+            earum totam magni reiciendis rem. Maiores obcaecati ab tenetur
+            consectetur, ducimus voluptates necessitatibus. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Voluptatum nostrum aperiam
+            eveniet assumenda praesentium tempore voluptates enim nam quia,
+            impedit sint? Beatae modi vel aperiam voluptatem ex. Pariatur, animi
+            magnam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Tenetur consectetur libero earum minima dignissimos eum. Accusantium
+            a mollitia, porro quibusdam minima quod laudantium nemo qui, non,
+            distinctio animi nulla inventore. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quae, vero? Minus aliquid molestias
+            autem, consectetur iure laborum fugiat dolorem minima, quas officia
+            cumque, modi tenetur. Ad molestias numquam ut ea. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Dolore fugiat consequatur ad
+            quidem sint dolorum deleniti fuga laudantium modi, laboriosam vel
+            optio rem unde animi iusto repellendus repudiandae. Doloremque,
+            optio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quod, quaerat! Libero illo delectus, nesciunt voluptatum dolores
+            nobis ab veritatis corporis dolor exercitationem iure eum aliquid
+            consectetur, ad necessitatibus molestiae incidunt!Lorem ipsum dolor
+            sit amet consectetur, adipisicing elit. Facilis molestias illo
+            perspiciatis, nihil expedita id excepturi distinctio, omnis quae ut,
+            ratione enim voluptas dolore sapiente cum nobis aspernatur mollitia
+            hic! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit molestias vel dignissimos, provident eligendi nihil
+            deserunt, ipsam, earum totam magni reiciendis rem. Maiores obcaecati
+            ab tenetur consectetur, ducimus voluptates necessitatibus. Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
+            nostrum aperiam eveniet assumenda praesentium tempore voluptates
+            enim nam quia, impedit sint? Beatae modi vel aperiam voluptatem ex.
+            Pariatur, animi magnam! Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Tenetur consectetur libero earum minima
+            dignissimos eum. Accusantium a mollitia, porro quibusdam minima quod
+            laudantium nemo qui, non, distinctio animi nulla inventore. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Quae, vero?
+            Minus aliquid molestias autem, consectetur iure laborum fugiat
+            dolorem minima, quas officia cumque, modi tenetur. Ad molestias
+            numquam ut ea. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Dolore fugiat consequatur ad quidem sint dolorum deleniti fuga
+            laudantium modi, laboriosam vel optio rem unde animi iusto
+            repellendus repudiandae. Doloremque, optio. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Quod, quaerat! Libero illo
+            delectus, nesciunt voluptatum dolores nobis ab veritatis corporis
+            dolor exercitationem iure eum aliquid consectetur, ad necessitatibus
+            molestiae incidunt!Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Facilis molestias illo perspiciatis, nihil
+            expedita id excepturi distinctio, omnis quae ut, ratione enim
+            voluptas dolore sapiente cum nobis aspernatur mollitia hic! Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+            molestias vel dignissimos, provident eligendi nihil deserunt, ipsam,
+            earum totam magni reiciendis rem. Maiores obcaecati ab tenetur
+            consectetur, ducimus voluptates necessitatibus. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Voluptatum nostrum aperiam
+            eveniet assumenda praesentium tempore voluptates enim nam quia,
+            impedit sint? Beatae modi vel aperiam voluptatem ex. Pariatur, animi
+            magnam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Tenetur consectetur libero earum minima dignissimos eum. Accusantium
+            a mollitia, porro quibusdam minima quod laudantium nemo qui, non,
+            distinctio animi nulla inventore. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quae, vero? Minus aliquid molestias
+            autem, consectetur iure laborum fugiat dolorem minima, quas officia
+            cumque, modi tenetur. Ad molestias numquam ut ea. Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Dolore fugiat consequatur ad
+            quidem sint dolorum deleniti fuga laudantium modi, laboriosam vel
+            optio rem unde animi iusto repellendus repudiandae. Doloremque,
+            optio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quod, quaerat! Libero illo delectus, nesciunt voluptatum dolores
+            nobis ab veritatis corporis dolor exercitationem iure eum aliquid
+            consectetur, ad necessitatibus molestiae incidunt!Lorem ipsum dolor
+            sit amet consectetur, adipisicing elit. Facilis molestias illo
+            perspiciatis, nihil expedita id excepturi distinctio, omnis quae ut,
+            ratione enim voluptas dolore sapiente cum nobis aspernatur mollitia
+            hic! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit molestias vel dignissimos, provident eligendi nihil
+            deserunt, ipsam, earum totam magni reiciendis rem. Maiores obcaecati
+            ab tenetur consectetur, ducimus voluptates necessitatibus. Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
+            nostrum aperiam eveniet assumenda praesentium tempore voluptates
+            enim nam quia, impedit sint? Beatae modi vel aperiam voluptatem ex.
+            Pariatur, animi magnam! Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Tenetur consectetur libero earum minima
+            dignissimos eum. Accusantium a mollitia, porro quibusdam minima quod
+            laudantium nemo qui, non, distinctio animi nulla inventore. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Quae, vero?
+            Minus aliquid molestias autem, consectetur iure laborum fugiat
+            dolorem minima, quas officia cumque, modi tenetur. Ad molestias
+            numquam ut ea. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Dolore fugiat consequatur ad quidem sint dolorum deleniti fuga
+            laudantium modi, laboriosam vel optio rem unde animi iusto
+            repellendus repudiandae. Doloremque, optio. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Quod, quaerat! Libero illo
+            delectus, nesciunt voluptatum dolores nobis ab veritatis corporis
+            dolor exercitationem iure eum aliquid consectetur, ad necessitatibus
+            molestiae incidunt!
+          </section>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
