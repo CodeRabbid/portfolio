509f7607e5933d5cb8ef0e6e323906775e5a8306
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-24 19:35:30 +0100

    fix styling of passage in about section

--- src/App.jsx
+++ src/App.jsx
@@ -82,24 +82,35 @@ function App() {
             <section id="about">
               <div className="section-header">ABOUT</div>
               <div className="content">
-                I’m a developer passionate about crafting accessible,
-                pixel-perfect user interfaces that blend thoughtful design with
-                robust engineering. My favorite work lies at the intersection of
-                design and development, creating experiences that not only look
-                great but are meticulously built for performance and usability.
-                Currently, I'm a Senior Front-End Engineer at Klaviyo,
-                specializing in accessibility. I contribute to the creation and
-                maintenance of UI components that power Klaviyo’s frontend,
-                ensuring our platform meets web accessibility standards and best
-                practices to deliver an inclusive user experience. In the past,
-                I've had the opportunity to develop software across a variety of
-                settings — from advertising agencies and large corporations to
-                start-ups and small digital product studios. Additionally, I
-                also released a comprehensive video course a few years ago,
-                guiding learners through building a web app with the Spotify
-                API. In my spare time, I’m usually climbing, reading, hanging
-                out with my wife and two cats, or running around Hyrule
-                searching for Korok seeds K o r o k s e e d s .
+                <p className="passage">
+                  I’m a developer passionate about crafting accessible,
+                  pixel-perfect user interfaces that blend thoughtful design
+                  with robust engineering. My favorite work lies at the
+                  intersection of design and development, creating experiences
+                  that not only look great but are meticulously built for
+                  performance and usability.
+                </p>
+                <p className="passage">
+                  Currently, I'm a Senior Front-End Engineer at Klaviyo,
+                  specializing in accessibility. I contribute to the creation
+                  and maintenance of UI components that power Klaviyo’s
+                  frontend, ensuring our platform meets web accessibility
+                  standards and best practices to deliver an inclusive user
+                  experience.
+                </p>
+                <p className="passage">
+                  In the past, I've had the opportunity to develop software
+                  across a variety of settings — from advertising agencies and
+                  large corporations to start-ups and small digital product
+                  studios. Additionally, I also released a comprehensive video
+                  course a few years ago, guiding learners through building a
+                  web app with the Spotify API.
+                </p>
+                <p className="passage">
+                  In my spare time, I’m usually climbing, reading, hanging out
+                  with my wife and two cats, or running around Hyrule searching
+                  for Korok seeds.
+                </p>
               </div>
             </section>
             <section id="experience">
