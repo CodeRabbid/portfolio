b9e682bd160d1673dfa3a8c28f467a139235fe32
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-26 20:33:36 +0100

    allow full screen on yt videos

--- src/App.jsx
+++ src/App.jsx
@@ -356,7 +356,7 @@ function App() {
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
+                    allowFullScreen
                   ></iframe>
                 </div>
               </div>
